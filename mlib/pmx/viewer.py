68e42abde11a1a8749ffcf7cb5d2a7b00874c8a2
miu <garnet200521358@gmail.com>
2023-03-25 22:09:03 +0900

    不要行削除

--- mlib/pmx/viewer.py
+++ mlib/pmx/viewer.py
@@ -40,37 +40,11 @@ class PmxCanvas(glcanvas.GLCanvas):
         self.motion = VmdMotion()
         self.bone_matrixes = np.array([np.eye(4) for _ in range(1)])
 
-        # self.model = PmxReader().read_by_filepath(pmx_path)
-
-        # self.model.init_draw(self.shader)
-
-        # self.motion = VmdReader().read_by_filepath(vmd_path) if vmd_path else VmdMotion()
-        # max_frame = self.motion.max_fno if self.motion else 100
-
-        # self.bone_matrixes = np.array([np.eye(4) for _ in range(len(self.model.bones))])
-
         # マウスドラッグフラグ
         self.is_drag = False
 
         # 再生中かどうかを示すフラグ
         self.playing = False
-
-        # # 非同期処理で演算した結果を格納する変数
-        # self.result = None
-
-        # # multiprocessing プロセス
-        # self.proc: Optional[Process] = None
-
-        # # IK計算対象
-        # self.ik_bf_indices: dict[str, list[int]] = {}
-        # if self.model and self.motion:
-        #     ik_bones = [bone for bone in self.model.bones if bone.is_ik]
-        #     for ik_bone in ik_bones:
-        #         self.ik_bf_indices[self.model.bones[ik_bone.index].name] = sorted(
-        #             list(set([bfs.index for bone in self.model.bone_trees[ik_bone.index] for bfs in self.motion.bones[bone.name]] + [self.motion.max_fno]))
-        #         )
-
-        # self.change_motion(wx.wxEVT_NULL)
 
     def _initialize_ui(self, parent):
         gl.glClearColor(0.7, 0.7, 0.7, 1)
@@ -94,9 +68,6 @@ class PmxCanvas(glcanvas.GLCanvas):
         self.Bind(wx.EVT_RIGHT_UP, self.on_mouse_up)
         self.Bind(wx.EVT_ERASE_BACKGROUND, self.on_erase_background)
         self.Bind(wx.EVT_KEY_DOWN, self.on_key_down)
-
-        # # タイマーイベントをバインド
-        # self.Bind(wx.EVT_TIMER, self.on_play_timer, self.play_timer)
 
         # タイマーイベントをバインド
         self.Bind(wx.EVT_TIMER, self.on_play_timer, self.play_timer)
@@ -236,24 +207,6 @@ class PmxCanvas(glcanvas.GLCanvas):
     def change_motion(self, event: wx.Event):
         if self.motion:
             now_fno = self.frame_ctrl.GetValue()
-            # for ik_bone_name, fnos in self.ik_bf_indices.items():
-            #     # IKがある場合、前計算を行っておく
-            #     prev_fnos = [fno for fno in fnos if now_fno >= fno]
-            #     prev_fno = prev_fnos[-1] if len(prev_fnos) > 0 else 0
-            #     next_fnos = [fno for fno in fnos if now_fno < fno]
-            #     next_fno = next_fnos[0] if len(next_fnos) > 0 else max(fnos)
-            #     self.motion.bones.get_matrix_by_indexes([prev_fno, next_fno], [self.model.bone_trees[self.model.bones[ik_bone_name].index]], self.model)
-
-            # # 非同期処理を実行する
-            # self.result = None
-            # self.proc = Process(
-            #     target=self.motion.bones.get_mesh_matrixes,
-            #     args=(
-            #         now_fno,
-            #         self.model,
-            #     ),
-            # )
-            # self.proc.start()
 
             self.bone_matrixes = self.motion.bones.get_mesh_matrixes(now_fno, self.model)
             self.Refresh()
@@ -280,13 +233,6 @@ class PmxCanvas(glcanvas.GLCanvas):
             matrixes_queue.put(bone_matrixes)
         matrixes_queue.put(None)
 
-    # def _get_mesh_matrixes_async(self, now_fno: int):
-    #     bone_matrixes = np.array([np.eye(4) for _ in range(len(self.model.bones))])
-    #     self.motion.bones.update_world_matrixes(bone_matrixes, now_fno)
-    #     self.motion.bones.update_skinning_matrixes(bone_matrixes, self.model.bones)
-    #     self.motion.update_morph(self.model.morphs, now_fno)
-    #     return bone_matrixes
-
     def on_capture(self, event: wx.Event):
         # キャプチャ
         # OpenGLの描画バッファを読み込む
@@ -310,17 +256,6 @@ class PmxCanvas(glcanvas.GLCanvas):
                 self.process.terminate()
         else:
             self.play_timer.Start(int(1000 / self.fps))  # タイマーを再開
-
-    # def on_load(self, event: wx.Event, pmx_path: str, motion_path: str):
-    #     if pmx_path:
-    #         self.model = PmxReader().read_by_filepath(pmx_path)
-    #         self.model.init_draw(self.shader)
-
-    #         if motion_path:
-    #             self.motion = VmdReader().read_by_filepath(motion_path)
-    #             self.bone_matrixes = np.array([np.eye(4) for _ in range(len(self.model.bones))])
-
-    #             self.change_motion(wx.wxEVT_NULL)
 
     def on_play_timer(self, event: wx.Event):
         if self.playing and self.model and self.motion:
