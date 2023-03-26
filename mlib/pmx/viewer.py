6f5c90e8c496dcc9dd172a7712a77cffcbfc48dd
miu <garnet200521358@gmail.com>
2023-03-26 14:16:26 +0900

    一旦シングルスレッドに戻した

--- mlib/pmx/viewer.py
+++ mlib/pmx/viewer.py
@@ -1,4 +1,4 @@
-from typing import Optional
+import time
 import OpenGL.GL as gl
 from OpenGL.GL import glReadPixels, GL_RGBA, GL_UNSIGNED_BYTE
 import wx
@@ -22,15 +22,9 @@ class PmxCanvas(glcanvas.GLCanvas):
         self.size = wx.Size(width, height)
         self.last_pos = wx.Point(0, 0)
         self.now_pos = wx.Point(0, 0)
-
-        self.SetCurrent(self.context)
-
-        self.matrixes_queue: Queue = Queue()
-        self.matrixes_process: Optional[Process] = None
         self.fps = 30
 
-        # 毎フレーム呼ばれるメソッド
-        self.on_update = self.get_mesh_matrixes_async
+        self.SetCurrent(self.context)
 
         self._initialize_ui(parent)
         self._initialize_ui_event()
@@ -56,20 +50,25 @@ class PmxCanvas(glcanvas.GLCanvas):
         self.play_timer = wx.Timer(self)
 
     def _initialize_ui_event(self):
+        # ペイントイベントをバインド
         self.Bind(wx.EVT_PAINT, self.on_paint)
+
+        # ウィンドウサイズ変更イベントをバインド
         self.Bind(wx.EVT_SIZE, self.on_resize)
+
+        # 背景を消すイベントをバインド
+        self.Bind(wx.EVT_ERASE_BACKGROUND, self.on_erase_background)
+
+        # タイマーイベントをバインド
+        self.Bind(wx.EVT_TIMER, self.on_play_timer, self.play_timer)
+
         self.Bind(wx.EVT_MOUSEWHEEL, self.on_mouse_wheel)
         self.Bind(wx.EVT_MIDDLE_DOWN, self.on_mouse_down)
         self.Bind(wx.EVT_RIGHT_DOWN, self.on_mouse_down)
         self.Bind(wx.EVT_MOTION, self.on_mouse_motion)
         self.Bind(wx.EVT_MIDDLE_UP, self.on_mouse_up)
         self.Bind(wx.EVT_RIGHT_UP, self.on_mouse_up)
-        self.Bind(wx.EVT_ERASE_BACKGROUND, self.on_erase_background)
         self.Bind(wx.EVT_KEY_DOWN, self.on_key_down)
-
-        # タイマーイベントをバインド
-        self.Bind(wx.EVT_TIMER, self.on_play_timer, self.play_timer)
-        self.Bind(wx.EVT_TIMER, self.on_update, self.play_timer)
 
     def on_erase_background(self, event: wx.Event):
         # Do nothing, to avoid flashing on MSW (これがないとチラつくらしい）
@@ -81,23 +80,10 @@ class PmxCanvas(glcanvas.GLCanvas):
         event.Skip()
 
     def on_paint(self, event: wx.Event):
-        self.draw(event)
-
+        self.draw()
         self.SwapBuffers()
 
-    def on_reset(self, event: wx.Event):
-        self.frame_ctrl.SetValue(0)
-        self.shader.vertical_degrees = self.shader.INITIAL_VERTICAL_DEGREES
-        self.shader.look_at_center = MVector3D(0, self.shader.INITIAL_LOOK_AT_CENTER_Y, 0)
-        self.shader.camera_rotation = MQuaternion()
-        self.shader.camera_position = MVector3D(
-            0,
-            self.shader.INITIAL_CAMERA_POSITION_Y,
-            self.shader.INITIAL_CAMERA_POSITION_Z,
-        )
-        self.Refresh()
-
-    def draw(self, event: wx.Event):
+    def draw(self):
         self.SetCurrent(self.context)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
 
@@ -110,6 +96,43 @@ class PmxCanvas(glcanvas.GLCanvas):
             self.shader.msaa.bind()
             self.model.draw(self.bone_matrixes)
             self.shader.msaa.unbind()
+
+    def on_frame_forward(self, event: wx.Event):
+        self.frame_ctrl.SetValue(self.frame_ctrl.GetValue() + 1)
+        self.change_motion(event)
+
+    def on_frame_back(self, event: wx.Event):
+        self.frame_ctrl.SetValue(max(0, self.frame_ctrl.GetValue() - 1))
+        self.change_motion(event)
+
+    def change_motion(self, event: wx.Event):
+        if self.motion:
+            now_fno = self.frame_ctrl.GetValue()
+            self.bone_matrixes = self.motion.bones.get_mesh_matrixes(now_fno, self.model)
+            self.Refresh()
+
+    def on_play(self, event):
+        self.playing = not self.playing  # フラグを反転
+        if not self.playing:
+            self.play_timer.Stop()  # タイマーを停止
+        else:
+            self.play_timer.Start(int(1000 / self.fps))  # タイマーを再開
+
+    def on_play_timer(self, event: wx.Event):
+        if self.playing and self.model and self.motion:
+            self.on_frame_forward(event)
+
+    def on_reset(self, event: wx.Event):
+        self.frame_ctrl.SetValue(0)
+        self.shader.vertical_degrees = self.shader.INITIAL_VERTICAL_DEGREES
+        self.shader.look_at_center = MVector3D(0, self.shader.INITIAL_LOOK_AT_CENTER_Y, 0)
+        self.shader.camera_rotation = MQuaternion()
+        self.shader.camera_position = MVector3D(
+            0,
+            self.shader.INITIAL_CAMERA_POSITION_Y,
+            self.shader.INITIAL_CAMERA_POSITION_Z,
+        )
+        self.Refresh()
 
     def on_key_down(self, event: wx.Event):
         keycode = event.GetKeyCode()
@@ -189,42 +212,6 @@ class PmxCanvas(glcanvas.GLCanvas):
             event.Skip()
         self.Refresh()
 
-    def on_frame_forward(self, event: wx.Event):
-        self.frame_ctrl.SetValue(self.frame_ctrl.GetValue() + 1)
-        self.change_motion(event)
-
-    def on_frame_back(self, event: wx.Event):
-        self.frame_ctrl.SetValue(max(0, self.frame_ctrl.GetValue() - 1))
-        self.change_motion(event)
-
-    def change_motion(self, event: wx.Event):
-        if self.motion:
-            now_fno = self.frame_ctrl.GetValue()
-
-            self.bone_matrixes = self.motion.bones.get_mesh_matrixes(now_fno, self.model)
-            self.Refresh()
-
-    def get_mesh_matrixes_async(self, event: wx.Event):
-        if not self.matrixes_process or not self.matrixes_process.is_alive():
-            self.matrixes_process = Process(
-                target=self.get_mesh_matrixes, args=(self.matrixes_queue, self.motion, self.model, self.frame_ctrl.GetValue()), name="MeshMatrixProcess"
-            )
-            self.matrixes_process.start()
-
-        if not self.matrixes_queue.empty():
-            bone_matrixes = self.matrixes_queue.get()
-            if bone_matrixes is not None:
-                self.bone_matrixes = bone_matrixes
-                self.frame_ctrl.SetValue(self.frame_ctrl.GetValue() + 1)
-
-    @staticmethod
-    def get_mesh_matrixes(matrixes_queue: Queue, motion: VmdMotion, model: PmxModel, now_fno: int):
-        while motion and motion.max_fno > now_fno:
-            now_fno += 1
-            bone_matrixes = motion.bones.get_mesh_matrixes(now_fno, model)
-            matrixes_queue.put(bone_matrixes)
-        matrixes_queue.put(None)
-
     def on_capture(self, event: wx.Event):
         # キャプチャ
         # OpenGLの描画バッファを読み込む
@@ -239,20 +226,6 @@ class PmxCanvas(glcanvas.GLCanvas):
         # ImageをPNGファイルとして保存する
         file_path = os.path.join(os.path.dirname(self.model.path), "capture.png")
         image.save(file_path)
-
-    def on_play(self, event: wx.Event):
-        self.playing = not self.playing  # フラグを反転
-        if not self.playing:
-            self.play_timer.Stop()  # タイマーを停止
-            if self.matrixes_process:
-                self.matrixes_process.terminate()
-        else:
-            self.play_timer.Start(int(1000 / self.fps))  # タイマーを再開
-
-    def on_play_timer(self, event: wx.Event):
-        if self.playing and self.model and self.motion:
-            self.frame_ctrl.SetValue(self.frame_ctrl.GetValue() + 1)
-            # self.on_frame_forward(event)
 
     def on_mouse_down(self, event: wx.Event):
         self.now_pos = self.last_pos = event.GetPosition()
