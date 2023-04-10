7e460e108f86515d7ecf7f9e2bff769dd572c0eb
miu <garnet200521358@gmail.com>
2023-04-10 22:28:04 +0900

    Update exception.py

--- mlib/base/exception.py
+++ mlib/base/exception.py
@@ -5,10 +5,9 @@ from typing import Optional
 class MLibException(Exception):
     """ライブラリ内基本エラー"""
 
-    def __init__(self, message: str = "", variants: list = [], exception: Optional[Exception] = None, *args):
-        super().__init__(*args)
+    def __init__(self, message: str = "", exception: Optional[Exception] = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.message = message
-        self.variants = variants
         self.exception = exception
 
     def __str__(self) -> str:
@@ -18,26 +17,26 @@ class MLibException(Exception):
 class MApplicationException(MLibException):
     """ツールがメイン処理出来なかった時のエラー"""
 
-    def __init__(self, message: str = "", variants: list = [], exception: Optional[Exception] = None, *args):
-        super().__init__(message, variants, exception, *args)
+    def __init__(self, message: str = "", exception: Optional[Exception] = None, *args, **kwargs):
+        super().__init__(message, exception, *args, **kwargs)
 
 
 class MParseException(MLibException):
     """ツールがパース出来なかった時のエラー"""
 
-    def __init__(self, message: str = "", variants: list = [], exception: Optional[Exception] = None, *args):
-        super().__init__(message, variants, exception, *args)
+    def __init__(self, message: str = "", exception: Optional[Exception] = None, *args, **kwargs):
+        super().__init__(message, exception, *args, **kwargs)
 
 
 class MViewerException(MLibException):
     """ツールが描画出来なかった時のエラー"""
 
-    def __init__(self, message: str = "", variants: list = [], exception: Optional[Exception] = None, *args):
-        super().__init__(message, variants, exception, *args)
+    def __init__(self, message: str = "", exception: Optional[Exception] = None, *args, **kwargs):
+        super().__init__(message, exception, *args, **kwargs)
 
 
 class MKilledException(MLibException):
     """ツールの実行が停止された時のエラー"""
 
-    def __init__(self, message: str = "", variants: list = [], exception: Optional[Exception] = None, *args):
-        super().__init__(message, variants, exception, *args)
+    def __init__(self, message: str = "", exception: Optional[Exception] = None, *args, **kwargs):
+        super().__init__(message, exception, *args, **kwargs)
