1b87d3e8242481658b5718f390a10985057b1430
miu <garnet200521358@gmail.com>
2023-05-06 02:32:10 +0900

    Update vmd_collection.py

--- mlib/vmd/vmd_collection.py
+++ mlib/vmd/vmd_collection.py
@@ -1181,7 +1181,7 @@ class VmdMotion(BaseHashModel):
         # ボーン変形行列
         matrixes = MMatrix4x4List(bone_poses.shape[0], bone_poses.shape[1])
         if not is_gl:
-            matrixes.translate([bone.position.vector for bone in model.bones if bone.index >= 0] + [np.zeros(3)])
+            matrixes.translate([(bone.position - model.bones[bone.parent_index].position).vector for bone in model.bones if bone.index >= 0] + [np.zeros(3)])
         matrixes.translate(bone_poses.tolist())
         matrixes.rotate(bone_qqs.tolist())
         matrixes.scale(bone_scales.tolist())
