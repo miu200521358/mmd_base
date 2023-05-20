7d12cd027ba461b3346ec589582766052ce776ef
miu <garnet200521358@gmail.com>
2023-05-20 16:28:42 +0900

    change_motionにmodel_index追加

--- mlib/pmx/canvas.py
+++ mlib/pmx/canvas.py
@@ -287,13 +287,16 @@ class PmxCanvas(glcanvas.GLCanvas):
         self.parent.fno = max(0, self.parent.fno - 1)
         self.change_motion(event)
 
-    def change_motion(self, event: wx.Event, is_bone_deform: bool = True):
+    def change_motion(self, event: wx.Event, is_bone_deform: bool = True, model_index: int = -1):
         if is_bone_deform:
-            animations: list[MotionSet] = []
-            for model_set in self.model_sets:
-                logger.debug(f"change_motion: MotionSet: {model_set.model.name}")
-                animations.append(MotionSet(model_set.model, model_set.motion, self.parent.fno))
-            self.animations = animations
+            if 0 > model_index:
+                animations: list[MotionSet] = []
+                for model_set in self.model_sets:
+                    logger.debug(f"change_motion: MotionSet: {model_set.model.name}")
+                    animations.append(MotionSet(model_set.model, model_set.motion, self.parent.fno))
+                self.animations = animations
+            else:
+                self.animations[model_index] = MotionSet(self.model_sets[model_index].model, self.model_sets[model_index].motion, self.parent.fno)
         else:
             for model_set, animation in zip(self.model_sets, self.animations):
                 logger.debug(f"change_motion: update_morphs: {model_set.model.name}")
