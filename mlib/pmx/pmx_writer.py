92f3f72ebc364db5b459a271f2c1b4587f11d35c
miu <garnet200521358@gmail.com>
2023-06-18 22:18:46 +0900

    Update pmx_writer.py

--- mlib/pmx/pmx_writer.py
+++ mlib/pmx/pmx_writer.py
@@ -48,8 +48,7 @@ class PmxWriter(BaseModel):
     def save(self) -> None:
         if not self.include_system:
             for bone_name in ("足中心", "首根元"):
-                if bone_name in self.model.bones:
-                    self.model.remove_bone(self.model.bones[bone_name])
+                self.model.remove_bone(bone_name)
 
         with open(self.output_path, "wb") as fout:
             target_bones = [b for b in self.model.bones if b.index >= 0] if self.include_system else self.model.bones.writable()
