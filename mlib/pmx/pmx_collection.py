fd049d7e38ae19b8b1d7b9b1f564f5fada818683
miu <garnet200521358@gmail.com>
2023-07-03 03:55:34 +0900

    不要な宣言削除

--- mlib/pmx/pmx_collection.py
+++ mlib/pmx/pmx_collection.py
@@ -97,8 +97,6 @@ class Bones(BaseIndexNameDictModel[Bone]):
         "cache",
         "indexes",
         "_names",
-        "_iter_index",
-        "_size",
         "is_bone_not_local_cancels",
         "local_axises",
         "parent_revert_matrixes",
@@ -1369,8 +1367,6 @@ class Meshes(BaseIndexDictModel[Mesh]):
     __slots__ = (
         "data",
         "indexes",
-        "_iter_index",
-        "_size",
         "shader",
         "model",
         "vertices",
