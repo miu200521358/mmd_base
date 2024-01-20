d937bcf1355b6a77dca2ffadcdd25bf886695f05
miu <38824651+miu200521358@users.noreply.github.com>
2024-01-20 18:02:50 +0900

    フォーマット

--- crumb/twist_party_hp.py
+++ crumb/twist_party_hp.py
@@ -74,17 +74,26 @@
 
 # ------------------------------------------------------
 import os
-from winsound import SND_ALIAS, PlaySound
-import pandas as pd
+from argparse import ArgumentParser
 from itertools import combinations, product
+from winsound import SND_ALIAS, PlaySound
+
+import pandas as pd
 from tqdm import tqdm
-import numpy as np
+
+parser = ArgumentParser()
+parser.add_argument("--character", type=str)
+parser.add_argument("--card_type", type=str)
+
+args, argv = parser.parse_known_args()
+
+main_card = {"カードキャラクター名": args.character, "カード種類": args.card_type}
 
 # ファイルを読み込む
-ssr_card_file_path = 'ツイステ：SSRカード一覧.csv'
+ssr_card_file_path = "ツイステ：SSRカード一覧.csv"
 ssr_card_df = pd.read_csv(ssr_card_file_path)
 
-party_file_path = 'ツイステ：編成組み合わせ.csv'
+party_file_path = "ツイステ：編成組み合わせ.csv"
 party_df = pd.read_csv(party_file_path)
 
 # # csvファイルの先頭数行を出力
@@ -92,55 +101,98 @@ party_df = pd.read_csv(party_file_path)
 
 # 出力用のDataFrameを初期化
 output_columns = [
-    "カード1", "カード2", "カード3", "カード4", "カード5", "HP合計", "ATK合計", "火属性数", "水属性数", "木属性数", "無属性数",
-    "デュオ数", "デュオ1", "デュオ2", "デュオ3", "デュオ4", "デュオ5",
-    "HP UP(中)数", "HP UP(中)1", "HP UP(中)2", "HP UP(中)3", "HP UP(中)4", "HP UP(中)5", "HP UP(中)6",
-    "HP UP(小)数", "HP UP(小)1", "HP UP(小)2", "HP UP(小)3", "HP UP(小)4", "HP UP(小)5", "HP UP(小)6",
-    "ATK UP(中)数", "ATK UP(中)1", "ATK UP(中)2", "ATK UP(中)3", "ATK UP(中)4", "ATK UP(中)5", "ATK UP(中)6",
-    "ATK UP(小)数", "ATK UP(小)1", "ATK UP(小)2", "ATK UP(小)3", "ATK UP(小)4", "ATK UP(小)5", "ATK UP(小)6"
+    "カード1",
+    "カード2",
+    "カード3",
+    "カード4",
+    "カード5",
+    "HP合計",
+    "ATK合計",
+    "火属性数",
+    "水属性数",
+    "木属性数",
+    "無属性数",
+    "デュオ数",
+    "HP UP(中)数",
+    "HP UP(小)数",
+    "ATK UP(中)数",
+    "ATK UP(小)数",
+    "デュオ1",
+    "デュオ2",
+    "デュオ3",
+    "デュオ4",
+    "デュオ5",
+    "HP UP(中)1",
+    "HP UP(中)2",
+    "HP UP(中)3",
+    "HP UP(中)4",
+    "HP UP(中)5",
+    "HP UP(中)6",
+    "HP UP(小)1",
+    "HP UP(小)2",
+    "HP UP(小)3",
+    "HP UP(小)4",
+    "HP UP(小)5",
+    "HP UP(小)6",
+    "ATK UP(中)1",
+    "ATK UP(中)2",
+    "ATK UP(中)3",
+    "ATK UP(中)4",
+    "ATK UP(中)5",
+    "ATK UP(中)6",
+    "ATK UP(小)1",
+    "ATK UP(小)2",
+    "ATK UP(小)3",
+    "ATK UP(小)4",
+    "ATK UP(小)5",
+    "ATK UP(小)6",
 ]
 output_df = pd.DataFrame(columns=output_columns)
+
 
 # 出力条件を満たすかチェックする関数
 def check_conditions(cards):
     # キャラクター名が重複していないかチェック
-    if len(set(cards['カードキャラクター名'])) != 5:
+    if len(set(cards["カードキャラクター名"])) != 5:
         return False
 
     # ATK合計のチェック
-    if cards['ATK'].sum() < 23000:
+    if cards["ATK"].sum() < 23000:
         return False
 
     # HP合計のチェック
-    if cards['HP'].sum() < 55000:
+    if cards["HP"].sum() < 55000:
         return False
 
     # 該当する編成におけるデュオ魔法、HP UP(中)、HP UP(小)の数を計算
-    counts = {
-        'デュオ魔法': 0,
-        'HP UP(中)': 0,
-        'HP UP(小)': 0
-    }
+    counts = {"デュオ魔法": 0, "HP UP(中)": 0, "HP UP(小)": 0}
 
     # デュオ魔法の数を計算
-    counts['デュオ魔法'] = sum(cards['デュオ魔法相手'].isin(cards['カードキャラクター名']))
+    counts["デュオ魔法"] = sum(cards["デュオ魔法相手"].isin(cards["カードキャラクター名"]))
 
-    if counts['デュオ魔法'] < 3:
+    if counts["デュオ魔法"] < 3:
         return False
 
     # HP UP(中)の数を計算
     # HP UP(小)の数を計算
     for k in range(1, 4):
-        buddy_skill = f'バディスキル{k}'
-        buddy_skill_partner = f'バディスキル{k}相手'
-        counts['HP UP(中)'] += sum((cards[buddy_skill] == 'HP UP(中)') & (cards[buddy_skill_partner].isin(cards['カードキャラクター名'])))
-        counts['HP UP(小)'] += sum((cards[buddy_skill] == 'HP UP(小)') & (cards[buddy_skill_partner].isin(cards['カードキャラクター名'])))
+        buddy_skill = f"バディスキル{k}"
+        buddy_skill_partner = f"バディスキル{k}相手"
+        counts["HP UP(中)"] += sum(
+            (cards[buddy_skill] == "HP UP(中)")
+            & (cards[buddy_skill_partner].isin(cards["カードキャラクター名"]))
+        )
+        counts["HP UP(小)"] += sum(
+            (cards[buddy_skill] == "HP UP(小)")
+            & (cards[buddy_skill_partner].isin(cards["カードキャラクター名"]))
+        )
 
     # 条件を満たしているかチェック
-    if (counts['HP UP(中)'] + (counts['HP UP(小)'] / 2)) >= 3:
+    if (counts["HP UP(中)"] + (counts["HP UP(小)"] / 2)) >= 3:
         return True
 
     return False
+
 
 # 条件を満たしている組み合わせを出力用DataFrameに追加する関数
 def add_to_output_df(selected_cards, output_df):
@@ -158,22 +210,22 @@ def add_to_output_df(selected_cards, output_df):
     for i in range(5):
         card_i = selected_cards.iloc[i]
 
-        if card_i['魔法1属性'] == '火':
+        if card_i["魔法1属性"] == "火":
             count_fire += 1
-        elif card_i['魔法1属性'] == '水':
+        elif card_i["魔法1属性"] == "水":
             count_water += 1
-        elif card_i['魔法1属性'] == '木':
+        elif card_i["魔法1属性"] == "木":
             count_green += 1
-        elif card_i['魔法1属性'] == '無':
+        elif card_i["魔法1属性"] == "無":
             count_none += 1
 
-        if card_i['魔法2属性'] == '火':
+        if card_i["魔法2属性"] == "火":
             count_fire += 1
-        elif card_i['魔法2属性'] == '水':
+        elif card_i["魔法2属性"] == "水":
             count_water += 1
-        elif card_i['魔法2属性'] == '木':
+        elif card_i["魔法2属性"] == "木":
             count_green += 1
-        elif card_i['魔法2属性'] == '無':
+        elif card_i["魔法2属性"] == "無":
             count_none += 1
 
         for j in range(5):
@@ -183,38 +235,50 @@ def add_to_output_df(selected_cards, output_df):
             card_j = selected_cards.iloc[j]
 
             # デュオ魔法のチェック
-            if card_i['デュオ魔法相手'] == card_j['カードキャラクター名']:
-                duos.append(f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}")
+            if card_i["デュオ魔法相手"] == card_j["カードキャラクター名"]:
+                duos.append(
+                    f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}"
+                )
 
             # バディスキルのチェック
             for k in range(1, 4):
-                buddy_skill = f'バディスキル{k}'
-                buddy_skill_partner = f'バディスキル{k}相手'
+                buddy_skill = f"バディスキル{k}"
+                buddy_skill_partner = f"バディスキル{k}相手"
 
-                if card_i[buddy_skill_partner] == card_j['カードキャラクター名']:
-                    if card_i[buddy_skill] == 'HP UP(中)':
-                        hp_up_medium.append(f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}")
-                    elif card_i[buddy_skill] == 'HP UP(小)':
-                        hp_up_small.append(f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}")
-                    elif card_i[buddy_skill] == 'ATK UP(中)':
-                        atk_up_medium.append(f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}")
-                    elif card_i[buddy_skill] == 'ATK UP(小)':
-                        atk_up_small.append(f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}")
+                if card_i[buddy_skill_partner] == card_j["カードキャラクター名"]:
+                    if card_i[buddy_skill] == "HP UP(中)":
+                        hp_up_medium.append(
+                            f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}"
+                        )
+                    elif card_i[buddy_skill] == "HP UP(小)":
+                        hp_up_small.append(
+                            f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}"
+                        )
+                    elif card_i[buddy_skill] == "ATK UP(中)":
+                        atk_up_medium.append(
+                            f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}"
+                        )
+                    elif card_i[buddy_skill] == "ATK UP(小)":
+                        atk_up_small.append(
+                            f"{card_i['カードキャラクター名']}-{card_i['カード種類']}&{card_j['カードキャラクター名']}-{card_j['カード種類']}"
+                        )
 
     # 出力用データを作成
     output_data = []
     for card in selected_cards.itertuples():
         output_data.extend([f"{card.カードキャラクター名}-{card.カード種類}"])
 
-    output_data.extend([
-        selected_cards['HP'].sum(),   # HP合計
-        selected_cards['ATK'].sum(),  # ATK合計
-        count_fire,                   # 火属性数
-        count_water,                  # 水属性数
-        count_green,                  # 木属性数
-        count_none,                   # 無属性数
-        len(duos)  # デュオ数
-    ])
+    output_data.extend(
+        [
+            selected_cards["HP"].sum(),  # HP合計
+            selected_cards["ATK"].sum(),  # ATK合計
+            count_fire,  # 火属性数
+            count_water,  # 水属性数
+            count_green,  # 木属性数
+            count_none,  # 無属性数
+            len(duos),  # デュオ数
+        ]
+    )
 
     # デュオ魔法の組み合わせを追加（最大5組）
     for i in range(5):
@@ -243,22 +307,52 @@ def add_to_output_df(selected_cards, output_df):
     # 出力用DataFrameにデータを追加
     output_df.loc[len(output_df)] = output_data
 
-main_card ={'カードキャラクター名': 'レオナ', 'カード種類': '寮服'}
-main_card_info = ssr_card_df[(ssr_card_df['カードキャラクター名'] == main_card['カードキャラクター名']) & (ssr_card_df['カード種類'] == main_card['カード種類'])]
+
+main_card_info = ssr_card_df[
+    (ssr_card_df["カードキャラクター名"] == main_card["カードキャラクター名"])
+    & (ssr_card_df["カード種類"] == main_card["カード種類"])
+]
 print(main_card)
 
 # メインカードの情報を取得
-main_card_character = main_card['カードキャラクター名']
-main_card_type = main_card['カード種類']
+main_card_character = main_card["カードキャラクター名"]
+main_card_type = main_card["カード種類"]
 dir_name = "party_hp"
 
 os.makedirs(dir_name, exist_ok=True)
 
 # キャラ名一覧
-character_names = ["リドル", "エース", "デュース", "トレイ", "ケイト", "レオナ", "ラギー", "ジャック", "アズール", "ジェイド", "フロイド", "カリム", "ジャミル", "ヴィル", "ルーク", "エペル", "イデア", "オルト", "マレウス", "リリア", "シルバー", "セベク"]
+character_names = [
+    "リドル",
+    "エース",
+    "デュース",
+    "トレイ",
+    "ケイト",
+    "レオナ",
+    "ラギー",
+    "ジャック",
+    "アズール",
+    "ジェイド",
+    "フロイド",
+    "カリム",
+    "ジャミル",
+    "ヴィル",
+    "ルーク",
+    "エペル",
+    "イデア",
+    "オルト",
+    "マレウス",
+    "リリア",
+    "シルバー",
+    "セベク",
+]
 
 # メインカードのキャラクター名以外のキャラ名を取得
-other_character_names = [character_name for character_name in character_names if character_name != main_card_character]
+other_character_names = [
+    character_name
+    for character_name in character_names
+    if character_name != main_card_character
+]
 
 # othersから4名のキャラクターを選択する全組み合わせを取得
 other_character_combinations = list(combinations(other_character_names, 4))
@@ -268,14 +362,40 @@ for n, other_characters in enumerate(tqdm(other_character_combinations)):
     card_combinations = [main_card_info.values.tolist()]
     for other_character in other_characters:
         # other_characterのカード一覧を取得
-        other_character_cards = ssr_card_df[(ssr_card_df['カードキャラクター名'] == other_character)]
+        other_character_cards = ssr_card_df[
+            (ssr_card_df["カードキャラクター名"] == other_character)
+        ]
         card_combinations.append(other_character_cards.values.tolist())
 
     # 5名のカード一覧を組み合わせる
-    character_products = product(card_combinations[0], card_combinations[1], card_combinations[2], card_combinations[3], card_combinations[4])
+    character_products = product(
+        card_combinations[0],
+        card_combinations[1],
+        card_combinations[2],
+        card_combinations[3],
+        card_combinations[4],
+    )
 
     for i, character_product in enumerate(character_products):
-        cards = pd.DataFrame(character_product, columns=['カードキャラクター名', 'カード種類', 'カード属性', '魔法1属性', '魔法2属性', 'HP', 'ATK', 'デュオ魔法相手', 'バディスキル1相手', 'バディスキル1', 'バディスキル2相手', 'バディスキル2', 'バディスキル3相手', 'バディスキル3'])
+        cards = pd.DataFrame(
+            character_product,
+            columns=[
+                "カードキャラクター名",
+                "カード種類",
+                "カード属性",
+                "魔法1属性",
+                "魔法2属性",
+                "HP",
+                "ATK",
+                "デュオ魔法相手",
+                "バディスキル1相手",
+                "バディスキル1",
+                "バディスキル2相手",
+                "バディスキル2",
+                "バディスキル3相手",
+                "バディスキル3",
+            ],
+        )
 
         if check_conditions(cards):
             add_to_output_df(cards, output_df)
@@ -284,13 +404,15 @@ for n, other_characters in enumerate(tqdm(other_character_combinations)):
     #     break
 
 # Sort output_df based on priority order
-output_df = output_df.sort_values(by=["HP合計", "HP UP(中)数", "HP UP(小)数", "デュオ数"], ascending=[False, False, False, False])
+output_df = output_df.sort_values(
+    by=["HP合計", "HP UP(中)数", "HP UP(小)数", "デュオ数"],
+    ascending=[False, False, False, False],
+)
 
 # 追加した出力用DataFrameのデータをcsvに出力
-output_df.to_csv(f'{dir_name}/{main_card_character}-{main_card_type}.csv', index=False)
+output_df.to_csv(f"{dir_name}/{main_card_character}-{main_card_type}.csv", index=False)
 
 PlaySound("SystemAsterisk", SND_ALIAS)
-
 
 
 # # SSR一覧からメインカードの情報を抽出
@@ -313,9 +435,6 @@ PlaySound("SystemAsterisk", SND_ALIAS)
 #         characters.add(character)
 #     if not duplicate:
 #         card_combinations.append(combination)
-
-
-
 
 
 # # カードの組み合わせを生成
@@ -342,19 +461,6 @@ PlaySound("SystemAsterisk", SND_ALIAS)
 # output_file_path = 'valid_combinations.csv'
 # output_df = pd.DataFrame(valid_combinations)
 # output_df.to_csv(output_file_path, index=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # top_10_rows = ssr_card_df.head(10)
